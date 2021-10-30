9ff5a0d9d66a250828c4563d91da409b49249742
Shane Orend <wagen00@gmail.com>
2021-10-30 16:34:05 -0400

    Add files via upload

---
+++ zoo.js
@@ -0,0 +1,33 @@
+
+//inventory object, item, quantity, date
+//health records object, animal data, employee, date, medicine/food
+//client object.events, ticketing, client data.
+var events[];
+var staff[];
+var inventoryItem[];
+var inventoryQty;
+function Scheduling(events,staff) {
+ if (staff.ava == 'Y') {
+ 	//asign to today's event
+ }
+}
+
+function inventory(inventoryQty,inventoryItem) {
+	if (inventoryQty<5) {
+		reOrderItem(inventoryItem){
+			var amountNeeded;
+		};
+	}
+}
+
+function healthRecords(animalId) {
+	if (animalid=="Match") {
+		//display animal health reccord.
+	}
+}
+function client(events) {
+var purchaseDate
+ if (purchaseDate== events[i].Date) {
+	//call Event schedule upsell query.
+ }
+}
