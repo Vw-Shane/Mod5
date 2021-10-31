ea278387a0bd4c35284d63717ff1c5f9daf092f5
Shane Orend <wagen00@gmail.com>
2021-10-31 09:40:00 -0400

    Add files via upload

--- zoo.js
+++ zoo.js
@@ -1,45 +1,71 @@
-
-//inventory object, item, quantity, date
-//health records object, animal data, employee, date, medicine/food
-//client object.events, ticketing, client data.
 var events[];
 var staff[];
 var inventoryItem[];
 var inventoryQty;
-function Scheduling(events,staff) {
- if (staff.ava == 'Y') {
- 	//asign to today's event
- }
+var Medicine
+
+function newEmployee() {
+    var Days;
+    var name;
+    var animalid;
 }
 
-function inventory(inventoryQty,inventoryItem) {
-	if (inventoryQty<5) {
-		reOrderItem(inventoryItem){
-			var amountNeeded;
-		};
-	}
+function admin() {
+    if (newEmployee = 'Y') {
+        newEmployee(days, name, animalid);
+    }
 }
+
+function user(name) {
+    timeOff();
+    animalHours();
+}
+
+function Scheduling() {
+    if (admin = 'Y') {
+        admin();
+    } else if (user = 'Y') {
+        user();
+    }
+    //asign to today's event
+}
+
+
+function inventory(inventoryQty, inventoryItem) {
+    var purchaseDate;
+    if (inventoryQty < 5) {
+        reOrderItem(inventoryItem) {
+            var amountNeeded;
+        };
+    }
+}
+
 
 function healthRecords(animalId) {
-	if (animalid=="Match") {
-		//display animal health reccord.
-	}
+    if (animalid == "Match") {
+        //display animal health reccord.
+    }
+
+    function importHealth() {
+        admin();
+        var fileFormat;
+    }
+    inventory(Medicine);
 }
+
 function client(events) {
-var purchaseDate
- if (purchaseDate== events[i].Date) {
-	//call Event schedule upsell query.
- }
+    var purchaseDate
+    if (purchaseDate == events[i].Date) {
+        //call Event schedule upsell query.
+    }
 }
 
 
 if (clientLogin == true) {
-	client();
+    client();
+} else if (userLogin == true) {
+    Scheduling();
+    if (animalHealth == true) {
+        healthRecords();
+    } else inventory();
 }
-else if (userLogin==true){
-	Scheduling();
-	if (animalHealth==true) {
-		healthRecords();
-	}else inventory();
-}
-
